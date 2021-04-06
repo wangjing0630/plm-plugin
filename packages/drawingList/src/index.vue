7e20af1073f3c115f3fe4a3bbbaecf5a8ece5933
wangjng <wang_jing@caxa.com>
2021-04-06 10:45:06 +0800

    '组件removeant'

--- packages/drawingList/src/index.vue
+++ packages/drawingList/src/index.vue
@@ -61,10 +61,10 @@
 </template>
 
 <script>
-import Vue from 'vue'
-import Antd from 'ant-design-vue';
-import 'ant-design-vue/dist/antd.css';
-Vue.use(Antd);
+// import Vue from 'vue'
+// import Antd from 'ant-design-vue';
+// import 'ant-design-vue/dist/antd.css';
+// Vue.use(Antd);
 const baselineColumns= [
   {
     title: "名称",
